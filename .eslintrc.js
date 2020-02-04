7f228a37ba466be0f642e56183f5024e0e00b70e
Mateus Castro <mateus.castro.domingos@gmail.com>
2020-02-04 17:31:04 -0300

    estilo globais configurados e estilização de autenticação finalizada

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'react/jsx-one-expression-per-line': 'off',
     'global-require': 'off',
     'react-native/no-raw-text': 'off',
+    'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
     'no-param-reassign': 'off',
     'no-underscore-dangle': 'off',
     camelcase: 'off',
